ed3bd795e8ce488cd48490bc12d1a16240cd0354
Jitt3ry <112400877+Jitt3ry@users.noreply.github.com>
2023-06-21 19:00:36 +0500

    [EDIT] project main to grid;

--- src/pages/ProjectMain.js
+++ src/pages/ProjectMain.js
@@ -22,66 +22,47 @@ const ProjectMain = () => {
         <div className="container">
             <div className="container-img">
                 <img src={project.mainImg} className="title-img-main" alt="dsa"></img>
-
                 <div className="arrow__container"><div className="arrows"></div></div>
-
             </div>
-            <div className="container-project">    
+
+
+            <div className="container-project">
                 <div className="project-main">
+                <div className="project-main-details">
+                    <ul className="main-info-list">
+                        <h2>Project Details</h2>
+                            <li>Roles: {project.roles}</li>
+                            <li>Semester: {project.semester}</li>
+                            <li>Team Size: {project.teamSize}</li>
+                            <li>Time Spent: {project.time}</li>
+                            <li>Date: {project.date}</li>
+                            <li>Engine: {project.engine}</li>
+                    </ul>
 
-                {/* INFO & BUTTONS & IMAGES */}
 
-                <div className="project-main-info-btns-img">
-                    
-                        {/* INFO & BUTTONS */}
-                        
-                        <div className="project-main-btns_with_info">
-    
-                            {/*  PROJECT DETAILS */}
-                            
-                            <div className="project-main-info">
-                                <ul className="main-info-list">
-                                    <h2>Project Details</h2>
-                                    <li>Roles: {project.roles}</li>
-                                    <li>Semester: {project.semester}</li>
-                                    <li>Team Size: {project.teamSize}</li>
-                                    <li>Time Spent: {project.time}</li>
-                                    <li>Date: {project.date}</li>
-                                    <li>Engine: {project.engine}</li>
-                                </ul>
-                            </div>
-    
-                        {/* BUTTONS */}
-                            
-                            <div className="project-main-buttons">
-                                {project.itch && (
-                                <a href={project.itch} target="_blank" rel="noreferrer" className="project-main-btn">
-                                    <img src={itchIcon} alt="itch-icon" />
-                                    <p>Go to itch.io</p>
-                                </a> )}
+                    <div className="project-main-buttons">
+                        {project.itch && (
+                            <a href={project.itch} target="_blank" rel="noreferrer" className="project-main-btn">
+                                <img src={itchIcon} alt="itch-icon" />
+                                <p>Go to itch.io</p>
+                            </a> )}
         
-                                {project.gitHub && (
-                                <a href={project.gitHub} target="_blank" rel="noreferrer" className="project-main-btn"> 
-                                    <img src={gitIcon} alt="gitHub-icon" />
-                                    <p>Go to Github</p>
-                                </a> )}
-                                
-                            </div>
-                            
-                        </div>
-    
-    
-                        <div className="project-main-images">
-                            <div className="descr-card-img">
-                                <img src={project.images[0]} alt="" />
-                            </div>                       
-                        </div>
-    
+                        {project.gitHub && (
+                            <a href={project.gitHub} target="_blank" rel="noreferrer" className="project-main-btn"> 
+                                <img src={gitIcon} alt="gitHub-icon" />
+                                <p>Go to Github</p>
+                            </a> )}        
+                    </div>
+
+                    <div className="project-main-images">
+                        <div className="descr-card-img">
+                            <img src={project.images[0]} alt="" />
+                        </div>                       
+                    </div>
                 </div>
-                    {/* DESCRIPTIONS */}
 
+                <div className="project-main-text">
                     <div className="project-main-descriptions">
-
                             <h2 className="title">Game Description</h2>
 
                             <div className="project-main-descr-card">
@@ -110,9 +91,10 @@ const ProjectMain = () => {
                             </div>
                     </div>
                 </div>
+                </div>
             </div>
         </div>
-     );
+    );
 }
  
 export default ProjectMain;
