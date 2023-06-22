c0c0eeeb132ca8adb82e558a6f05d4fb7ee82e19
Alan Berberov <92302861+psikh286@users.noreply.github.com>
2023-06-23 00:17:49 +0200

    [EDIT] Header layout;

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,16 +11,16 @@ const Header = () => {
                         <p className="header__info-gamedev">Game Developer & technical designer</p>
                     </div>
                 </div>
-            <div className="header__doombar">
-                <div className="header__doombar-list">
-                    <div className="doombar-list-main"> <p className="list-main-num">84%</p> <p className="list-main-text">C#</p></div>
-                    <div className="doombar-list-main"> <p className="list-main-num">67%</p> <p className="list-main-text">Design System</p></div>
-                    <div className="doombar-list-main"> <p className="list-main-text">place for photo??</p></div>
-                    <div className="doombar-list-main"> <p className="list-main-num">96%</p><p className="list-main-text">Sociable</p></div>
-                    <div className="doombar-list-main"> <p className="list-main-num">88%</p><p className="list-main-text">GitHub</p></div>
-                    <div className="doombar-list-main"> <p className="list-main-num">90%</p><p className="list-main-text">Unity</p></div>
+                <div className="header__doombar">
+                    <div className="header__doombar-list">
+                        {/*<div className="doombar-list-main"> <p className="list-main-num">84%</p> <p className="list-main-text">C#</p></div>
+                        <div className="doombar-list-main"> <p className="list-main-num">67%</p> <p className="list-main-text">Design System</p></div>
+                        <div className="doombar-list-main"> <p className="list-main-text">place for photo??</p></div>
+                        <div className="doombar-list-main"> <p className="list-main-num">96%</p><p className="list-main-text">Sociable</p></div>
+                        <div className="doombar-list-main"> <p className="list-main-num">88%</p><p className="list-main-text">GitHub</p></div>
+                        <div className="doombar-list-main"> <p className="list-main-num">90%</p><p className="list-main-text">Unity</p></div>*/}
+                    </div>
                 </div>
-            </div>
             </div>
         </Element>
      );
