d2926dd113c4377c53a97461c247d3b7e092e9e5
Alan Berberov <92302861+psikh286@users.noreply.github.com>
2023-07-01 00:05:12 +0200

    [EDIT] project page and small fixes;

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,36 +1,36 @@
 //region Imports
 
-/*GRAVITY*/
+//GRAVITY
 import gravityCover from '../img/gravity/Gravity_cover.png'
 import gravityMainImage from '../img/gravity/Gravity_main.png'
 import gravityImage0 from '../img/gravity/Gravity_1.png'
 import gravityImage1 from '../img/gravity/Gravity_2.png'
 
-/*SLIMY*/
+//SLIMY
 import slimyCover from '../img/slimy/Slimy_cover.png'
 import slimyMainImage from '../img/slimy/Slimy_main.png'
 import slimyImage0 from '../img/slimy/Slimy_0.png'
 import slimyImage1 from '../img/slimy/Slimy_1.png'
 
-/*EOTW*/
+//EOTW
 import eotwCover from '../img/eotw/EOTW_cover.png'
 import eotwMainImage from '../img/eotw/EOTW_main.png'
 import eotwImage0 from '../img/eotw/EOTW_1.png'
 import eotwImage1 from '../img/eotw/EOTW_2.png'
 
-/*PARTY CHAOS*/
+//PARTY CHAOS
 import partyChaosCover from '../img/partyChaos/PartyChaos_cover.png'
 import partyChaosMainImage from '../img/partyChaos/PartyChaos_0.png'
 import partyChaosImage0 from '../img/partyChaos/PartyChaos_1.png'
 import partyChaosImage1 from '../img/partyChaos/PartyChaos_5.png'
 
-/*SOUND OF THE ROAD*/
-import soundOfTheRoadCover from '../img/soundOfTheRoad/SoundOfTheRoad_cover.png'
-import soundOfTheRoadMainImage from '../img/soundOfTheRoad/SoundOfTheRoad_cover.png'
-import soundOfTheRoadImage0 from '../img/soundOfTheRoad/SoundOfTheRoad_cover.png'
-import soundOfTheRoadImage1 from '../img/soundOfTheRoad/SoundOfTheRoad_cover.png'
+//SOUND OF THE ROAD
+import soundOfTheRoadCover from '../img/soundOfTheRoad/sotr_cover.png'
+import soundOfTheRoadMainImage from '../img/soundOfTheRoad/sotr_main.png'
+import soundOfTheRoadImage0 from '../img/soundOfTheRoad/sotr_1.png'
+import soundOfTheRoadImage1 from '../img/soundOfTheRoad/sotr_2.png'
 
-/*GENETICS*/
+//GENETICS
 /*import geneticsCover from '../img/genetics/Genetics_cover.png'
 import geneticsMainImage from '../img/genetics/Genetics_main.png'
 import geneticsImage0 from '../img/genetics/Genetics_1.png'
@@ -40,17 +40,23 @@ import geneticsMainImage from '../img/blank.png'
 import geneticsImage0 from '../img/blank.png'
 import geneticsImage1 from '../img/blank.png'
 
-/*CRAFT*/
+//CRAFT
 import craftingCover from '../img/crafting/Craft_cover.png'
 import craftingMainImage from '../img/blank.png'
 import craftingImage0 from '../img/crafting/Craft_1.png'
 import craftingImage1 from '../img/crafting/Craft_2.png'
 
-/*PARKOUR DEMO*/
+//PARKOUR DEMO
 import parkourCover from '../img/parkour/parkour_cover.png'
 import parkourMainImage from '../img/parkour/parkour_main.png'
 import parkourImage0 from '../img/parkour/parkour_1.png'
 import parkourImage1 from '../img/parkour/parkour_0.png'
+
+//TRIBE SIM
+import tribeCover from '../img/tribe/Tribe_cover.png'
+import tribeMainImage from '../img/tribe/Tribe_main.png'
+import tribeImage0 from '../img/tribe/Tribe_1.png'
+import tribeImage1 from '../img/tribe/Tribe_2.png'
 
 
 //endregion
@@ -60,7 +66,7 @@ const projects = [
         //ON MAIN PAGE
         "title" : "Tribe Sim",
         "coverInfo" : '<h2>TRIBE SIM</h2>',
-        "cover" : partyChaosCover,
+        "cover" : tribeCover,
         "contribution" : '<li>Behavior Tree</li><li>Tick System</li><li>Inheritance System</li><li>Agent&#39s Behavior</li><li>Agent&#39s Memory</li><li>Simulation Control System</li>',
 
         //DETAILS
@@ -76,23 +82,23 @@ const projects = [
         "itch": "https://psikh286.itch.io/party-chaos",
         
         //IMAGES
-        "mainImg" : partyChaosMainImage,
+        "mainImg" : tribeMainImage,
         "images" : [
-            partyChaosImage0,
-            partyChaosImage1            
+            tribeImage0,
+            tribeImage1            
         ],
         
         //TEXT
-        "info" : 'Local multiplayer party game with the support of 2-8 people and a bunch of fun minigames. It is a “Pass the Controller” type game. <br/><br/> <i> I would love to answer your questions! However, I don’t want to share the code source.</i>',
-        "problems" : '<ol><li>Players\' orders might feel repetitive over time.</li><li>Minigames order decided by pure random might lead to unfair outcomes while keeping order the same is repetitive. (old version of the game)</li><li>Create a procedurally generated Hub where players can pick the next game and make everything appealing.</li></ol>',
-        "solutions" : '<ol><li>I implemented a shuffle to ensure that each person plays against every other person before reshuffling. And players can’t play 2 games in a row (except if it’s 1 player team).</li><li> We divided our games on the “type” of skills(fast/luck/skill-based) and the emotions that they invoke. After we created templates of which type of games should be played next. And I made a system that uses those templates to pick random but suitable games. (old version of the game)</li><li>I recreated mini versions(previews) of each minigame in the Hub, where players can test it and vote if they like it. After a minigame is played, the preview turns gray until all minigames are played. And to make everything a bit cooler, added Lego building stile animations for previews and a bunch of physics objects to mess around with.</li></ol>',
-    },
+        "info" : '',
+        "problems": '',
+        "solutions" : ''
+        },
     {
         //ON MAIN PAGE
         "title" : "Party Chaos",
         "coverInfo" : '<h2>PARTY CHAOS</h2>',
         "cover" : partyChaosCover,
-        "contribution" : '<li>Team System</li><li>Minigames Logic</li><li>Procedurally Generated Hub </li><li>Minigames Design</li><li>Level Design</li><li>Post-Processing</li><li>Board Game System (v0.1)</li>',
+        "contribution" : '<li>Team System</li><li>Minigames Logic</li><li>Procedurally Generated Hub </li><li>Minigames Design</li><li>Level Design</li><li>Post-Processing FX</li><li>Board Game System (v0.1)</li>',
 
         //DETAILS
         "roles" : "Programmer, Game Designer",
@@ -114,9 +120,9 @@ const projects = [
         ],
 
         //TEXT
-        "info" : 'Local multiplayer party game with the support of 2-8 people and a bunch of fun minigames. It is a “Pass the Controller” type game. <br/><br/> <i> I would love to answer your questions! However, I don’t want to share the code source.</i>',
-        "problems" : '<ol><li>Players\' orders might feel repetitive over time.</li><li>Minigames order decided by pure random might lead to unfair outcomes while keeping order the same is repetitive. (old version of the game)</li><li>Create a procedurally generated Hub where players can pick the next game and make everything appealing.</li></ol>',
-        "solutions" : '<ol><li>I implemented a shuffle to ensure that each person plays against every other person before reshuffling. And players can’t play 2 games in a row (except if it’s 1 player team).</li><li> We divided our games on the “type” of skills(fast/luck/skill-based) and the emotions that they invoke. After we created templates of which type of games should be played next. And I made a system that uses those templates to pick random but suitable games. (old version of the game)</li><li>I recreated mini versions(previews) of each minigame in the Hub, where players can test it and vote if they like it. After a minigame is played, the preview turns gray until all minigames are played. And to make everything a bit cooler, added Lego building stile animations for previews and a bunch of physics objects to mess around with.</li></ol>',
+        "info" : 'Local multiplayer party game with the support of 2-8 people and a bunch of fun minigames. It is a “Pass the Controller” type game.',
+        "problems" : 'Minigames order decided by pure random might lead to unfair outcomes while keeping order the same is repetitive. (old version of the game)',
+        "solutions" : 'We divided our games on the “type” of skills(fast/luck/skill-based) and the emotions that they invoke. After we created templates of which type of games should be played next. And I made a system that uses those templates to pick random but suitable games. (old version of the game)',
     },
     {
         //ON MAIN PAGE
@@ -148,6 +154,38 @@ const projects = [
         "info" : 'Inspired by <a href="https://www.youtube.com/watch?v=raQ3iHhE_Kk" target="_blank" rel="noreferrer">this talk</a>. The project is an experiment with <a href="https://en.wikipedia.org/wiki/Mendelian_inheritance" target="_blank" rel="noreferrer">simple inheritance patterns</a> and aims to be designer-friendly by allowing easy modifications through the Unity inspector without opening the code editor. It can be expanded without extra refactoring.',
         "problems" : "Keep everything simple for the user and, at the same time, have complex functionality.",
         "solutions" : "I divided everything into chunks, so traits, relations between each other, and a list of those traits(genotypes) are separate objects in Unity Inspector. It also makes everything modular and reusable. I used Scriptable Objects as a bridge between designers and programmers. "
+    },
+
+
+    {
+        //ON MAIN PAGE
+        "title" : "Sound Of the Road",
+        "coverInfo" : 'How I made a casual goofy <h2>CAR CONTROLLER</h2>',
+        "cover" : soundOfTheRoadCover,
+
+        //DETAILS
+        "roles" : "Programmer, Game Designer",
+        "semester": "4th",
+        "teamSize": "9(actually 7)",
+        "time": "40+ hours",
+        "date": "On-going",
+        "engine": "Unity Engine",
+
+        //BUTTONS
+        "gitHub": null,
+        "itch": "https://psikh286.itch.io/sound-of-the-road",
+
+        //IMAGES
+        "mainImg" : soundOfTheRoadMainImage,
+        "images" : [
+            soundOfTheRoadImage0,
+            soundOfTheRoadImage1
+        ],
+
+        //TEXT
+        "info" : "Casual car controls, a radio, and a low-poly, colorful, and post-apocalyptic world. YES! This game still doesn&#39t have a clear gameplay goal, but the experience is already there. My last student project(so far) is with an incredible team of 9, but actually 7 people.",
+        "problems" : 'I had to make a casual fun car controller, but I didn&#39t have experience in car games.',
+        "solutions" : 'I played some and created a custom physics car controller by applying different forces at 4 points without simulating actual wheels. Also made an option to switch and create custom car settings runtime.'
     },
     {
         //ON MAIN PAGE
@@ -181,6 +219,36 @@ const projects = [
     },
     {
         //ON MAIN PAGE
+        "title" : "Parkour Demo",
+        "coverInfo" : 'My <h2>FIRST</h2> experience in <h2>UNREAL ENGINE</h2>',
+        "cover" : parkourCover,
+
+        //DETAILS
+        "roles" : "Programmer, Game Designer",
+        "semester": "3rd",
+        "teamSize": "2",
+        "time": "20-25 hours",
+        "date": "18.11.2022",
+        "engine": "Unreal Engine",
+
+        //BUTTONS
+        "gitHub": "https://github.com/psikh286/NarrativeJuice",
+        "itch": null,
+
+        //IMAGES
+        "mainImg" : parkourMainImage,
+        "images" : [
+            parkourImage0,
+            parkourImage1
+        ],
+
+        //TEXT
+        "info" : "As a 1st experience in Unreal Engine, we built a parkour movement demo with features like wall running, sliding, air dash, and more. We used the <a href='https://www.unrealengine.com/marketplace/en-US/product/advanced-locomotion-system-v1' target='_blank' rel='noreferrer'>ALSv4 package</a> as a foundation.",
+        "problems" : "The main problem was that I didn&#39t have an experience with Unreal.",
+        "solutions" : "Learning Unreal from scratch wasn&#39t possible, so I started to define similarities and differences with Unity. After a while, I had a clearer picture of how to implement stuff, and I used my Bolt (Visual Scripting in Unity) knowledge gained in the 1st semester to make wall-running and air-dash.\n"
+    },
+    {
+        //ON MAIN PAGE
         "title" : "Slimy",
         "coverInfo" : 'My <h2>FIRST SOLO</h2> Unity Project',
         "cover" : slimyCover,
@@ -208,36 +276,6 @@ const projects = [
         "info" : "",
         "problems" : "aboba",
         "solutions" : "aboba"
-    },
-    {
-        //ON MAIN PAGE
-        "title" : "Sound Of the Road",
-        "coverInfo" : 'How I made a casual goofy <h2>CAR CONTROLLER</h2>',
-        "cover" : soundOfTheRoadCover,
-
-        //DETAILS
-        "roles" : "Programmer, Game Designer",
-        "semester": "4th",
-        "teamSize": "9(actually 7)",
-        "time": "40+ hours",
-        "date": "On-going",
-        "engine": "Unity Engine",
-
-        //BUTTONS
-        "gitHub": null,
-        "itch": "https://psikh286.itch.io/sound-of-the-road",
-
-        //IMAGES
-        "mainImg" : soundOfTheRoadMainImage,
-        "images" : [
-            soundOfTheRoadImage0,
-            soundOfTheRoadImage1
-        ],
-
-        //TEXT
-        "info" : "Casual car controls, a radio, and a low-poly, colorful, and post-apocalyptic world. YES! This game still doesn’t have a clear gameplay goal, but the experience is already there. My last student project(so far) is with an incredible team of 9, but actually 7 people.",
-        "problems" : '<ol><li>I had to make a casual fun car controller, but I didn’t have experience in car games.</li><li>We needed a radio (zone-specific) and CD record-player system.</li></ol>',
-        "solutions" : '<ol><li>I played some and created a custom physics car controller by applying different forces at 4 points without simulating actual wheels. Also made an option to switch and create custom car settings runtime.</li><li>I merged them, made an opportunity to create/switch discs with songs runtime, and made radio as an actual disc that changes based on position in the world.</li></ol>'
     },
     {
         //ON MAIN PAGE
@@ -298,36 +336,6 @@ const projects = [
         "info" : "Student project where I create a high-pace crafting game that becomes messy quickly. Craft required items while new materials pop up every second.",
         "problems" : "Every element in the game is UI, and due to a lack of knowledge back then, it was a big challenge to make everything work.",
         "solutions" : "There is no fancy solution here. I've read a bunch of Unity docs and made it work." 
-    },
-    {
-        //ON MAIN PAGE
-        "title" : "Parkour Demo",
-        "coverInfo" : 'My <h2>FIRST</h2> experience in <h2>UNREAL ENGINE</h2>',
-        "cover" : parkourCover,
-
-        //DETAILS
-        "roles" : "Programmer, Game Designer",
-        "semester": "3rd",
-        "teamSize": "2",
-        "time": "20-25 hours",
-        "date": "18.11.2022",
-        "engine": "Unreal Engine",
-
-        //BUTTONS
-        "gitHub": "https://github.com/psikh286/NarrativeJuice",
-        "itch": null,
-
-        //IMAGES
-        "mainImg" : parkourMainImage,
-        "images" : [
-            parkourImage0,
-            parkourImage1
-        ],
-
-        //TEXT
-        "info" : "As a 1st experience in Unreal Engine, we built a parkour movement demo with features like wall running, sliding, air dash, and more. We used the <a href='https://www.unrealengine.com/marketplace/en-US/product/advanced-locomotion-system-v1' target='_blank' rel='noreferrer'>ALSv4 package</a> as a foundation.",
-        "problems" : "The main problem was that I didn’t have an experience with Unreal.",
-        "solutions" : "Learning Unreal from scratch wasn’t possible, so I started to define similarities and differences with Unity. After a while, I had a clearer picture of how to implement stuff, and I used my Bolt (Visual Scripting in Unity) knowledge gained in the 1st semester to make wall-running and air-dash.\n"
     },
 ];
 
--- src/pages/ProjectMain.js
+++ src/pages/ProjectMain.js
@@ -12,6 +12,7 @@ import '../styles/downScrollButton.css'
 import arrowIcon from '../img/upButton.svg'
 import gitIcon from '../img/linkLogos/github.svg'
 import itchIcon from '../img/linkLogos/itch-io.svg'
+import ScrollButton from "../components/utils/ScrollButton";
     
 const ProjectMain = () => {
     const {id} = useParams();
@@ -32,62 +33,66 @@ const ProjectMain = () => {
             <div className="project__cover">
                 <NavLink to ={'/'} className="project__back-button">Go Back</NavLink>
                 <img src={project.mainImg} className="project__cover-img" alt="scroll down"></img>
-                <div className="down-button down" onClick={handleScrollToBlock}>
-                    <div className="down-button__body">
-                        <img src={arrowIcon} alt="scrollDown"/>
-                    </div>
-                    <div className="down-button__effect"></div>
-                </div>
+                <ScrollButton dir={"down"} dest={"project__body"}/>
             </div>
 
             <div className="project__body">
-                <div className="project__content-container">
-                    <ul className="project__details">
-                        <div className="project__details-wrapper">
-                            <h2>Project Details</h2>
-                            <li dangerouslySetInnerHTML={{__html: '<b>Roles: </b>' + project.roles}}></li>
-                            {project.semester &&
-                                <li dangerouslySetInnerHTML={{__html: '<b>Semester: </b>' + project.semester}}></li>}
-                            <li dangerouslySetInnerHTML={{__html: '<b>Team Size: </b>' + project.teamSize}}></li>
-                            <li dangerouslySetInnerHTML={{__html: '<b>Time Spent: </b>' + project.time}}></li>
-                            <li dangerouslySetInnerHTML={{__html: '<b>Date: </b>' + project.date}}></li>
-                            <li dangerouslySetInnerHTML={{__html: '<b>Engine: </b>' + project.engine}}></li>
-                        </div>
-                    </ul>
-
-                    <div className="project__buttons-container">
-                        {project.itch && (
-                            <a href={project.itch} target="_blank" rel="noreferrer" className="project__button">
-                                <img src={itchIcon} alt="itch-icon" className="project__icon" />
-                                <p>Play it!</p>
-                            </a> )}
-
-                        {project.gitHub && (
-                            <a href={project.gitHub} target="_blank" rel="noreferrer" className="project__button">
-                                <img src={gitIcon} alt="gitHub-icon" className="project__icon" />
-                                <p>Code!</p>
-                            </a> )}
-                    </div>
-
-                    <div className="project__image">
+                <div className="project__left-container">
+                    <div className="project__images block">
                         <img src={project.images[0]} alt="" />
                     </div>
+                    <div className="project__images block">
+                        <img src={project.images[1]} alt="" />
+                    </div>
                 </div>
-                <div className="project__main--container">
-                    <div className="project-main-descr-card">
-                        <h2>Game Description</h2>
-                        <div className="descr-text" dangerouslySetInnerHTML={{__html: project.info}}></div>
-                    </div>
+                <div className="project__right--container">
 
-                    <div className="project-main-descr-card">
-                        <h2>Problems</h2>
-                        <div className="descr-text" dangerouslySetInnerHTML={{__html: project.problems}}></div>
-                    </div>
+                    <ul className="project__details block">
 
-                    <div className="project-main-descr-card">
-                        <h2>Solutions</h2>
-                        <div className="descr-text" dangerouslySetInnerHTML={{__html: project.solutions}}></div>
+                        <h2>PROJECT DETAILS</h2>
+                        <li dangerouslySetInnerHTML={{__html: '<b>Roles: </b>' + project.roles}}></li>
+                        {project.semester &&
+                            <li dangerouslySetInnerHTML={{__html: '<b>Semester: </b>' + project.semester}}></li>}
+                        <li dangerouslySetInnerHTML={{__html: '<b>Team Size: </b>' + project.teamSize}}></li>
+                        <li dangerouslySetInnerHTML={{__html: '<b>Time Spent: </b>' + project.time}}></li>
+                        <li dangerouslySetInnerHTML={{__html: '<b>Date: </b>' + project.date}}></li>
+                        <li dangerouslySetInnerHTML={{__html: '<b>Engine: </b>' + project.engine}}></li>
+                        
+                    </ul>
+
+                    <div className="portfolio__wid block">
+                        <h2>MY CONTRIBUTIONS</h2>
+                        <ul dangerouslySetInnerHTML={{__html:project.contribution}}></ul>
+                        <div className="portfolio__buttons">
+                            {project.gitHub && (
+                                <a href={project.gitHub} target="_blank" rel="noreferrer" className="portfolio__button">
+                                    <img src={gitIcon} alt="gitHub-icon" className="project__icon" />
+                                    <p>Code!</p>
+                                </a> )}
+
+                            {project.itch && (
+                                <a href={project.itch} target="_blank" rel="noreferrer" className="portfolio__button">
+                                    <img src={itchIcon} alt="itch-icon" className="project__icon" />
+                                    <p>Play it!</p>
+                                </a> )}
+                        </div>
                     </div>
+                    
+                    <div className="project__text-container ">
+                        <div className="project__info block project__text">
+                            <h2>Game Description</h2>
+                            <div dangerouslySetInnerHTML={{__html: project.info}}></div>
+                        </div>
+                        <div className="project__problems block project__text">
+                            <h2>Problem</h2>
+                            <div dangerouslySetInnerHTML={{__html: project.problems}}></div>
+                        </div>
+                        <div className="project__solutions block project__text">
+                            <h2>Solution</h2>
+                            <div dangerouslySetInnerHTML={{__html: project.solutions}}></div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
             
